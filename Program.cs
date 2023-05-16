a59b1b1d6014d8188f89e3c86bfc5398be789f18
BMAG-tests <steffanleprince@gmail.com>
2023-05-16 17:53:18 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Globalization;
+
+public class Solution
+{
+
+
+    public static void Main()
+    {
+
+
+        int result = BirthdayCandles();
+        Console.Write(result);
+       
+    }
+
+    public static int BirthdayCandles()
+    {
+
+        int[] candles = new int [4]{ 2, 4, 4, 1 };
+
+        int result = 0;
+        int max = 0;
+
+        for (int i = 0; i < candles.Length; i++)
+        {
+            int j = 1;
+            if (candles[i] >= candles[j])
+            {
+                max = candles[i];
+            }
+
+            j++;
+        }
+
+        for (int i = 0; i < candles.Length; i++)
+        {
+            if (candles[i] >= max)
+            {
+                result++;
+            }
+        }
+
+
+
+            return result;
+    }
+
+
+
+}
